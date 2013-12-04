a1b97207b71da198f922597c0a8ad096186502e2
EDWARD LU <evl5051@athertn108-lab-mac147.clc-labs.its.psu.edu>
2013-12-04 13:04:52 -0500

    Added groups for files

---
+++ Dota Companion/ELUHeroesModel.h
@@ -0,0 +1,15 @@
+//
+//  ELUHeroesModel.h
+//  Dota Companion
+//
+//  Created by EDWARD LU on 12/4/13.
+//  Copyright (c) 2013 EDWARD LU. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ELUHeroesModel : NSObject
+
+- (id) initWithJsonFile: (NSString*) jsonFile;
+
+@end
---
+++ Dota Companion/ELUHeroesModel.m
@@ -0,0 +1,21 @@
+//
+//  ELUHeroesModel.m
+//  Dota Companion
+//
+//  Created by EDWARD LU on 12/4/13.
+//  Copyright (c) 2013 EDWARD LU. All rights reserved.
+//
+
+#import "ELUHeroesModel.h"
+
+@interface ELUHeroesModel ()
+
+@end
+
+@implementation ELUHeroesModel
+
+- (id) initWithJsonFile: (NSString*) jsonFile {
+    
+}
+
+@end
