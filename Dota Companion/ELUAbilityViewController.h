a24b972fe86b24e0b1fae118f2cf5c6bde8e7a15
EDWARD LU <evl5051@athertn108-lab-mac147.clc-labs.its.psu.edu>
2013-12-10 14:44:53 -0500

    Added ability view controller class

---
+++ Dota Companion/ELUAbilityViewController.h
@@ -0,0 +1,13 @@
+//
+//  ELUAbilityViewController.h
+//  Dota Companion
+//
+//  Created by EDWARD LU on 12/9/13.
+//  Copyright (c) 2013 EDWARD LU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ELUAbilityViewController : UIViewController
+
+@end
---
+++ Dota Companion/ELUAbilityViewController.m
@@ -0,0 +1,38 @@
+//
+//  ELUAbilityViewController.m
+//  Dota Companion
+//
+//  Created by EDWARD LU on 12/9/13.
+//  Copyright (c) 2013 EDWARD LU. All rights reserved.
+//
+
+#import "ELUAbilityViewController.h"
+
+@interface ELUAbilityViewController ()
+
+@end
+
+@implementation ELUAbilityViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
